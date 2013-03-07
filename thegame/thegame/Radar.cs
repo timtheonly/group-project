31a167a19ab0007d201b9c169e8c53b81348db39
daniel hogan <timtheonly@gmail.com>
2013-03-07 16:37:19 +0000

    improved Radar

--- thegame/thegame/Radar.cs
+++ thegame/thegame/Radar.cs
@@ -26,6 +26,7 @@ namespace thegame
 
         private const float radarScreenRadius = 61.0f;
         private static Vector2 radarCenterPos = new Vector2(700, 400);
+        private Vector2 playerPos;
         private Vector2 imageCenter;
         private Vector2 differanceVect;
 
@@ -44,7 +45,7 @@ namespace thegame
             imageCenter = new Vector2(radarLayer.Width * 0.5f, radarLayer.Height * 0.5f);
         }
 
-        public override void Update(Microsoft.Xna.Framework.GameTime gameTime)
+        public  void Update(GameTime gameTime)
         {
             differanceVect = new Vector2(Game1.getInstance().getEnemy().getPos().X - Game1.getInstance().getPlayer().getPos().X, Game1.getInstance().getEnemy().getPos().Z - Game1.getInstance().getPlayer().getPos().Z);
             float distance = differanceVect.LengthSquared();
@@ -58,7 +59,9 @@ namespace thegame
 
                 // Offset coords from radar's center
                 differanceVect += radarCenterPos;
-                
+                playerPos = new Vector2(Game1.getInstance().getPlayer().getPos().X, Game1.getInstance().getPlayer().getPos().Z);
+                playerPos *= radarScreenRadius / radarRange;
+                playerPos += radarCenterPos;
             }
             base.Update(gameTime);
         }
@@ -68,9 +71,9 @@ namespace thegame
             Game1.getInstance().getSpriteBatch().Draw(radarLayer, radarCenterPos, null, Color.White, 0.0f, imageCenter, radarScreenRadius / (radarLayer.Height * 0.5f), SpriteEffects.None, 0.0f);
             if (Game1.getInstance().getEnemy().isAlive())
             {
-                Game1.getInstance().getSpriteBatch().Draw(enemyDot, new Vector2(differanceVect.X - (playerDot.Width * 0.5f), differanceVect.Y - (playerDot.Width * 0.5f)), null, Color.White, 0.0f, new Vector2(0.0f, 0.0f), 1.0f, SpriteEffects.None, 0.0f);
+                Game1.getInstance().getSpriteBatch().Draw(enemyDot, new Vector2(differanceVect.X - (enemyDot.Width * 0.5f), differanceVect.Y - (enemyDot.Width * 0.5f)), null, Color.White, 0.0f, new Vector2(0.0f, 0.0f), 1.0f, SpriteEffects.None, 0.0f);
             }
-            Game1.getInstance().getSpriteBatch().Draw(playerDot, new Vector2(radarCenterPos.X-(playerDot.Width *0.5f),radarCenterPos.Y-(playerDot.Width *0.5f)), Color.White);
+            Game1.getInstance().getSpriteBatch().Draw(playerDot, new Vector2((playerPos.X)-(playerDot.Width *0.5f),playerPos.Y-(playerDot.Width *0.5f)), Color.White);
             //base.Draw(gameTime);
         } 
     }
